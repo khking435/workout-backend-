1ef5984e29ef0c65f6fa7b560f2eec52080ac3d8
Sumnicky99 <sumnicky99@gmail.com>
2024-07-10 15:32:56 +0300

    updated app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def Fit_fussion():
-    return 'Hello, Fitness a bWorld!'
+    return 'Hello, Fitness World!'
 
 if __name__ == '__main__':
     app.run(debug=True)
