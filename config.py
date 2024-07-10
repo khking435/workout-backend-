d7360e4c7aefc0d9ce68d6da644a72939371bb69
Gift <gzomandi07@gmail.com>
2024-07-10 23:09:31 +0300

    configuaration variables

--- config.py
+++ config.py
@@ -4,3 +4,10 @@ import os
 class Config:
     DEBUG = True
     # Add other configuration variables here
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = DATABASE
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
