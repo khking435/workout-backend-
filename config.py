84eb221bb6f86a9a619f1df139b386362d7d9525
Gift <gzomandi07@gmail.com>
2024-07-10 18:55:24 +0300

    database uri

--- config.py
+++ config.py
@@ -4,3 +4,7 @@ import os
 class Config:
     DEBUG = True
     # Add other configuration variables here
+
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///your_database_name.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
