7277e9c5951cdc470ef685aef5a2253bcedcd260
Sumnicky99 <sumnicky99@gmail.com>
2024-07-11 09:32:01 +0300

    doing importation in db.py

--- database models/db.py
+++ database models/db.py
@@ -1,8 +1,8 @@
 # Import the Flask class to create a Flask application
-from flask import Flask  
+from flask import Flask
 
 # Import SQLAlchemy for database handling
-from flask_sqlalchemy import SQLAlchemy  
+from flask_sqlalchemy import SQLAlchemy
 
 # Import Migrate for handling database migrations
-from flask_migrate import Migrate  
+from flask_migrate import Migrate
