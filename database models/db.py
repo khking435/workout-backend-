c1d762b816822cba42b160c66b7c704494b1f2a5
Sumnicky99 <sumnicky99@gmail.com>
2024-07-11 09:35:21 +0300

    doing exe models  in db.py

--- database models/db.py
+++ database models/db.py
@@ -6,3 +6,18 @@ from flask_sqlalchemy import SQLAlchemy
 
 # Import Migrate for handling database migrations
 from flask_migrate import Migrate
+
+ # Create an instance of the Flask class
+app = Flask(__name__) 
+
+# Set the URI for the database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///workout.db'  
+
+# Create a SQLAlchemy object and link it to the Flask app
+db = SQLAlchemy(app)  
+
+# Create a Migrate object to handle migrations
+migrate = Migrate(app, db)  
+
+# Define the Exercise model
+class Exercise(db.Model):
